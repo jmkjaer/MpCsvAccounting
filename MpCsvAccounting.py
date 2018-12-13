481628eb8e7fcaf9a66d60e24e83f9d135b14b9a
jmkjaer <jmkjaer@hotmail.com>
2018-12-13 22:52:16 +0100

    Docstrings

--- MpCsvAccounting.py
+++ MpCsvAccounting.py
@@ -9,6 +9,8 @@ HOLIDAYS_DK = holidays.DK()
 
 
 class Account(str):
+    """String constants for Dinero's bank accounts."""
+
     BANK = "55000"
     GAVEKORT = "63080"
     GEBYRER = "7220"
@@ -16,6 +18,8 @@ class Account(str):
 
 
 def prepareCsvReader(filePath):
+    """Prepares the CSV reader by opening the file and skipping two lines."""
+
     file = open(filePath, "r", newline="", encoding="utf-16-le")
     next(file)
     next(file)
@@ -24,6 +28,16 @@ def prepareCsvReader(filePath):
 
 
 def readTransactionsFromFile(filePath):
+    """Returns transactions in batches read from the CSV file exported by MP.
+    
+    We are not interested in the bank transactions or the fees listed by MP, \
+    only the actual user transactions (sales, or "Salg"), since they will be \
+    manipulated and used for calculation later.
+    
+    Returns a list of lists of user transactions bundled in transaction \
+    batches before bank transfer.
+    """
+
     reader = prepareCsvReader(filePath)
     transactions = []
     transactionsByBatch = []
@@ -43,28 +57,44 @@ def readTransactionsFromFile(filePath):
             continue
         else:
             raise ValueError("Unknown transaction type")
-    # The imported CSV ends with a bunch of sales with no "Overførsel"
+    # The imported CSV ends with a batch of sales with no "Overførsel"
     transactionsByBatch.append(transactions)
 
     return transactionsByBatch
 
 
 def prepareCsvWriter(filePath):
+    """Prepares the CSV writer by opening the file and setting delimiters."""
+
     file = open(filePath, "w", newline="")
     csvWriter = csv.writer(file, delimiter=";")
     return csvWriter
 
 
 def floatToLocalStringDecimal(float):
+    """Replaces decimal point with comma, used in import and export CSV."""
+
     return str(float).replace(".", ",")
 
 
 def isRegistration(transaction):
+    """Finds out if the transaction is part of a registration.
+    
+    Searches for the substrings "tilmeld" and "indmeld" in the MP \
+    transaction comment.
+    """
+
     return "tilmeld" in transaction[2].lower() or "indmeld" in transaction[2].lower()
 
 
-def calculateBatchInfo(batch):
-    registrationFee = 200
+def calculateBatchInfo(batch, registrationFee=200):
+    """Returns important information from a batch of transactions.
+    
+    Calculates and returns the amount transferred to the bank, the fees by \
+    MP, the registration fees paid by the members, and the voucher amount for \
+    the members.
+    """
+
     registrationFees = 0
     voucherAmount = 0
     mpFees = 0
@@ -88,6 +118,8 @@ def calculateBatchInfo(batch):
 
 # https://stackoverflow.com/a/42824111
 def nextBusinessDay(date):
+    """Returns the next business day for bank transfer."""
+
     nextDay = date.date() + datetime.timedelta(days=1)
     while nextDay.weekday() in holidays.WEEKEND or nextDay in HOLIDAYS_DK:
         nextDay += datetime.timedelta(days=1)
@@ -96,7 +128,12 @@ def nextBusinessDay(date):
 
 
 def writeTransactions(filePath, appendixStart, transactionsByBatch):
-    currTransferIndex = 0
+    """Writes the information gathered throughout the script to a CSV file.
+    
+    The resulting CSV file is recognized by Dinero's journal entry CSV \
+    import. The written information is in Danish.
+    """
+
     currAppendix = appendixStart
     csvWriter = prepareCsvWriter(filePath)
 
@@ -155,6 +192,12 @@ def writeTransactions(filePath, appendixStart, transactionsByBatch):
 
 
 def main():
+    """Reads a CSV by MP and writes a CSV recognizable by Dinero.
+    
+    The locale is set to be able to convert the Danish decimal numbers into \
+    floats instead of replacing the separator ourselves. May be removed in \
+    the future.
+    """
     locale.setlocale(locale.LC_NUMERIC, "en_DK.UTF-8")
 
     filename = input("File name (from " + str(Path.cwd()) + "):\n> ")
